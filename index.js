94a71a18e3527aa6f037f4dfa95a62f8cb51c2b6
EjuMir <mirejo101@gmail.com>
2024-05-11 15:12:26 +0600

    problem solved on bad auth

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config;
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 //book-server
 //GOUwNHkws2cLggAM
 
-app.use(cors(
-    {
-        origin:['http://localhost:5173/']
-    }
-))
+app.use(cors())
 app.use(express.json())
 
-
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.cckizs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cckizs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,14 +30,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 }); 
+    const bookCollection = client.db('GoBook').collection('BookCollection');
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    
-    const bookCollection = client.db("GoBook").collection("BookCollection");
 
     app.get('/allBooks', async(req, res)=>{
-        const findAll = bookCollection.find();
-        const result = await findAll.toArray();
+        const cursor = bookCollection.find();
+        const result = await cursor.toArray();
         res.send(result);
     })
 
