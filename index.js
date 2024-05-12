d781e7ec4de768d6cbfa2d1b20b9541b3a7947ad
EjuMir <mirejo101@gmail.com>
2024-05-13 03:49:47 +0600

    put and post operation for borrowed books status:200

--- index.js
+++ index.js
@@ -31,64 +31,92 @@ async function run() {
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-    app.get('/allBooks', async(req, res)=>{
-        const cursor = bookCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/allBooks', async (req, res) => {
+      const cursor = bookCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.post('/allBooks', async(req, res)=>{
-        const data = req.body;
-        const result = await bookCollection.insertOne(data);
-        res.send(result);
+    app.post('/allBooks', async (req, res) => {
+      const data = req.body;
+      const result = await bookCollection.insertOne(data);
+      res.send(result);
     })
 
-    app.get('/allBooks/:category', async(req, res) => {
-        const category = req.params.category;
-        const query = {category : category};
-        const result = await bookCollection.find(query).toArray();
-        res.send(result);
+    app.get('/allBooks/:category', async (req, res) => {
+      const category = req.params.category;
+      const query = { category: category };
+      const result = await bookCollection.find(query).toArray();
+      res.send(result);
     })
 
-    app.get('/details', async(req, res) => {
-        const find = bookCollection.find();
-        const result = await find.toArray();
-        res.send(result);
+    app.get('/details', async (req, res) => {
+      const find = bookCollection.find();
+      const result = await find.toArray();
+      res.send(result);
     })
-    app.get('/details/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await bookCollection.findOne(query);
-        res.send(result)
+    app.get('/details/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.post('/borrowedBooks', async (req, res) => {
+      const book = req.body;
+      const result = await borrowedBooks.insertOne(book);
+      res.send(result);
     })
 
     //borrowed books route
-    app.post('/borrowedBooks', async(req, res)=>{
+    app.put('/borrowedBooks', async (req, res) => {
       const book = req.body;
-      const add = await borrowedBooks.insertOne(book);
-      res.send(add);
+      const modify = await bookCollection.updateOne({
+        name : book.name
+      },{
+        $inc : {
+          quantity : -1
+        } 
+      }
+    )
+      res.send(modify);
     })
 
-    app.get('/borrowedBooks', async(req, res)=>{
+    
+
+    app.get('/borrowedBooks', async (req, res) => {
       let query = {}
-      console.log(req.query); 
-       if(req.query?.email){
-        query = {email: req.query.email};
-       }
-       const find = await borrowedBooks.find(query).toArray();
-       res.send(find);
+      console.log(req.query);
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const find = await borrowedBooks.find(query).toArray();
+      res.send(find);
     })
 
+    //put operation on quantity
+    app.put('/allBooks', async (req, res) => {
+      const datas = req.body;
+      const options = { upsert: true };
+      const filter = {name : datas.name}
+      const updateDoc = {
+        $set: {
+           quantity : amount.newQuantity
+        },
+      };
+      const result = await bookCollection.updateOne(filter, updateDoc, options)
+      console.log(result);
+    })
 
 
     //Category section
-    app.get('/category', async(req, res)=>{
+    app.get('/category', async (req, res) => {
       const cursor = category.find();
       const result = await cursor.toArray();
       res.send(result);
     })
 
-    
+
 
   } finally {
     // Ensures that the client will close when you finish/error
@@ -99,9 +127,9 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req,res)=>{
-    res.send('Book server is running')
+app.get('/', (req, res) => {
+  res.send('Book server is running')
 })
-app.listen(port, ()=>{
-    console.log(`server is running at port ${port}`);
+app.listen(port, () => {
+  console.log(`server is running at port ${port}`);
 })
