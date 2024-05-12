6d10c8bb74fe337b3b316bebfe12e6ba2ece447b
EjuMir <mirejo101@gmail.com>
2024-05-13 00:57:55 +0600

    made a route and collection for borrowed books

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cckizs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,7 @@ async function run() {
     // await client.db("admin").command({ ping: 1 }); 
     const bookCollection = client.db('GoBook').collection('BookCollection');
     const category = client.db('GoBook').collection('Category');
+    const borrowedBooks = client.db('GoBook').collection('BorrowedBooks');
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
@@ -42,18 +43,42 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/allBooks/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {category : id};
+    app.get('/allBooks/:category', async(req, res) => {
+        const category = req.params.category;
+        const query = {category : category};
         const result = await bookCollection.find(query).toArray();
         res.send(result);
     })
+
+    app.get('/details', async(req, res) => {
+        const find = bookCollection.find();
+        const result = await find.toArray();
+        res.send(result);
+    })
+    app.get('/details/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await bookCollection.findOne(query);
+        res.send(result)
+    })
+
+    //borrowed books route
+    app.post('/borrowedBooks', async(req, res)=>{
+      const book = req.body;
+      const add = await borrowedBooks.insertOne(book);
+      res.send(add);
+    })
+
+
+
     //Category section
     app.get('/category', async(req, res)=>{
       const cursor = category.find();
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    
 
   } finally {
     // Ensures that the client will close when you finish/error
