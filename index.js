f2ebebf1d01731328fc6e8254d572ab5d0a69712
EjuMir <mirejo101@gmail.com>
2024-05-14 09:08:07 +0600

    finale

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,9 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(cors({
   origin:["https://localhost:5173"],
   credentials: true
-}))
+}));
+app.use(cookieParser());
+app.use(express.json());
 
-app.use(express.json())
+//middlewares
+const verifyUser = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (token) {
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401).send('Unauthorized Access');
+      } else {
+        req.user = decoded;
+        next();
+      }
+    })
+  } else {
+    res.status(401).send('Unauthorized Access');
+  }
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cckizs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -64,7 +82,8 @@ async function run() {
       res.send(result);
     })
 
-    app.post('/allBooks', async (req, res) => {
+    app.post('/allBooks', verifyUser, async (req, res) => {
+     console.log('token owner', req.user);
       const data = req.body;
       const result = await bookCollection.insertOne(data);
       res.send(result);
